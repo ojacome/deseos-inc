cd7a59aa2b0a45b1c67d67af686e92fb23f8cfd0
olmedo <olmedo,bdp@gmail.com>
2021-01-15 16:57:58 -0500

    alert y localStorage

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { DeseosService } from 'src/app/services/deseos.service';
 
 @Component({
@@ -11,10 +12,47 @@ export class Tab1Page {
 
   constructor(
     public deseoSvc: DeseosService, 
-    private router: Router
+    private router: Router,
+    public alertController: AlertController
   ) { }
 
-  agregarLista(){
-    this.router.navigateByUrl('/tabs/tab1/agregar')
+
+  
+  async agregarLista(){
+
+    
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Nueva lista',
+      inputs: [
+        {
+          name:         'titulo',
+          type:         'text',
+          placeholder:  'Nombre de la lista'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.info('cancelar');
+          }
+        },
+        {
+          text: 'Crear',
+          handler: (data: any) => {
+            console.info(data);
+            if( data.titulo.length < 2 ){ return }
+
+            //crear la lista
+            this.deseoSvc.crearLista(data.titulo);
+          }
+        }
+      ]
+    });
+
+    alert.present();
+    // this.router.navigateByUrl('/tabs/tab1/agregar')
   }
 }
--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -13,11 +13,30 @@ export class DeseosService {
 
   constructor() { 
 
-    const lista1 = new Lista('Recoletar piedras del infinito');
-    const lista2 = new Lista('Heroes a desaparecer');
-
-    this.listas.push(lista1, lista2);
-
+    this.cargarStorage();
   }
 
+
+
+  crearLista(titulo: string){
+
+    const newList = new Lista( titulo );
+    this.listas.push( newList );
+    this.guardarStorage();
+  }
+
+  guardarStorage() {
+
+    localStorage.setItem('data', JSON.stringify( this.listas ) );
+  }
+
+  cargarStorage() {
+
+    if( localStorage.getItem('data') ){
+      this.listas = JSON.parse( localStorage.getItem('data') );
+    }
+    else{
+      this.listas = [];
+    }
+  }
 }
